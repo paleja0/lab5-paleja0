b73eda32b957d8f14969ba69ba83a2e4c2714135
Paula García <paulagarro@unisabana.edu.co>
2026-03-15 17:59:59 -0500

    1 sync

--- functions.py
+++ functions.py
@@ -1,8 +1,8 @@
-def promedio_estudiante(calificaciones):
+def promedio_estudiante (calificaciones):
     if not calificaciones:
         return 0.0
-    suma_total = sum(calificaciones)
-    cantidad = len(calificaciones)
-    promedio = float(suma_total / cantidad)
+    suma_total = sum (calificaciones)
+    cantidad = len (calificaciones)
+    promedio = float (suma_total / cantidad)
     
     return promedio
