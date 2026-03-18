f2a22755a7b10c60a5bde0298e63411caa7c02e9
Paula García <paulagarro@unisabana.edu.co>
2026-03-17 23:03:42 -0500

    :(

--- return_types.py
+++ return_types.py
@@ -1,7 +1,3 @@
 def obtener_precio_usuario():
-
-    precio = input("Enter the item's price:\n")
-    precio = float(precio)
-    return precio
-precio = obtener_precio_usuario()
-print(precio) 
+    precio = input("Enter the item's price: ")
+    return float(precio)
